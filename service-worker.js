64a2c0b4303c6b229b687eb7f31385c8ba86fd52
piervit82-dotcom <piervit82@gmail.com>
2026-06-18 14:28:55 +0200

    EssereInRegola: cache v20

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
    I dispositivi scaricheranno la nuova versione e cancelleranno
    la cache vecchia al riavvio successivo.
    ========================================================= */
-const CACHE = 'inregola-v19';
+const CACHE = 'inregola-v20';
 const ASSETS = [
   './',
   './index.html',
